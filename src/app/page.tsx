e8f96fb3bfe3ebbdf6004692f6d1060a3e3762f5
Theophilus <isahtheophilus@gmail.com>
2023-07-13 15:28:55 +0100

    feat: initialise project

---
+++ src/app/page.tsx
@@ -0,0 +1,93 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started with&nbsp;
+          <code className="font-mono font-bold">Amavas</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <div
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          >
+            By{" "}
+            <p>
+              Amavas Technologies, (supported by{" "}
+              <a
+                className="text-sky-300 hover:underline"
+                target="_blank"
+                href="https:\\qala.dev"
+              >
+                Qala
+              </a>
+              )
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-orange before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-orange-200 after:via-orange-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-orange-700 before:dark:opacity-10 after:dark:from-orange-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+        <code className="font-mono font-bold text-2xl">Amavas</code>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <p className="font-mono font-bold">A merchant <span className="text-orange-500">Bitcoin</span> POS solution for businesses</p>
+        <code>Coming soon!</code>
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
+        <a
+          href="#"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={"mb-3 text-2xl font-semibold"}>
+            Instant Payments{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={"m-0 max-w-[30ch] text-sm opacity-50"}>
+            No more waiting for payments to clear. Get your money instantly.
+          </p>
+        </a>
+
+        <a
+          href="#"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={"mb-3 text-2xl font-semibold"}>
+            Fiat Settlement{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={"m-0 max-w-[30ch] text-sm opacity-50"}>
+            Easily settle and withdraw your funds (Bitcoin and fiat) in your local currency.
+          </p>
+        </a>
+
+        <a
+          href="#"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={"mb-3 text-2xl font-semibold"}>
+            POS Accounts Integration{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={"m-0 max-w-[30ch] text-sm opacity-50"}>
+            See all your transactions on the go with our accounting dashboard.
+          </p>
+        </a>
+
+       
+      </div>
+    </main>
+  );
+}
